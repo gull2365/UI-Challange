eca96a06261080b5cfa4dd023f0a54b444e97ee0
ojyochan <yohan1101@oj.vision>
2024-06-27 16:38:05 +0900

    end : pomodoro app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:toonflix/widget/button.dart';
-import 'package:toonflix/widget/currency_card.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -13,128 +11,17 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: const Color(0xFF181818),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 80,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            cardColor: const Color(0xFFE7626C),
+          ),
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Color(0xFF232B55),
             ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Hey, Selena',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800),
-                    ),
-                    Text(
-                      'Welcome back',
-                      style: TextStyle(
-                        color: Color.fromRGBO(255, 255, 255, 0.8),
-                        fontSize: 18,
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            const Text(
-              "Total Balance",
-              style: TextStyle(
-                  color: Color.fromARGB(204, 255, 255, 255), fontSize: 22),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "\$5 194 482",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 48,
-                  fontWeight: FontWeight.w700),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Button(
-                    text: "Transfer",
-                    bgColor: Color(0xFFF1B33B),
-                    textColor: Colors.black),
-                Button(
-                    text: "Request",
-                    bgColor: Color(0xFF1F2133),
-                    textColor: Colors.white)
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            const Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Wallets",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 36,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  "View All",
-                  style: TextStyle(
-                    color: Color.fromARGB(114, 255, 255, 255),
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CurrencyCard(
-              name: "Euro",
-              code: "EUR",
-              amount: "6,428",
-              icon: Icons.euro_rounded,
-              isInverted: false,
-              order: 1,
-            ),
-            CurrencyCard(
-              name: 'Bitcoin',
-              code: 'BTC',
-              amount: '9 785',
-              icon: Icons.currency_bitcoin_rounded,
-              isInverted: true,
-              order: 2,
-            ),
-            CurrencyCard(
-              name: 'Dollar',
-              code: 'USD',
-              amount: '428',
-              icon: Icons.attach_money_rounded,
-              order: 3,
-            ),
-          ],
+          ),
+          cardColor: const Color(0xFFF4EDDB),
         ),
-      ),
-    ));
+        home: const HomeScreen());
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,153 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes;
+  int totalPomodoros = 0;
+  bool isRunning = false;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros = totalPomodoros + 1;
+        isRunning = false;
+        totalSeconds = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1), // 1초마다 호출
+      onTick,
+    );
+    setState(() {
+      isRunning = !isRunning; // false -> true
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = !isRunning; // ture? false
+    });
+  }
+
+  void onResetPressed() {
+    timer.cancel();
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+      isRunning = false;
+    });
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    return duration.toString().split(".").first.substring(2, 7);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                format(totalSeconds),
+                style: TextStyle(
+                    color: Theme.of(context).cardColor,
+                    fontSize: 89,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      isRunning
+                          ? Icons.pause_circle_outline
+                          : Icons.play_circle_outline,
+                      color: Theme.of(context).cardColor,
+                      size: 98,
+                    ),
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.restore,
+                      color: Theme.of(context).cardColor,
+                      size: 50,
+                    ),
+                    onPressed: onResetPressed,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Pomodoro",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                          ),
+                        ),
+                        Text(
+                          '$totalPomodoros',
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontWeight: FontWeight.w600,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
